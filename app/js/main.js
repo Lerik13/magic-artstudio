30bdef1821816864494a78f0d5141d8552ee7d6d
Lerik13 <45076280+Lerik13@users.noreply.github.com>
2020-03-14 12:03:18 -0600

    Header

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.header__slider').slick({    
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrow_slider-yellow.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrow_slider-yellow.svg" alt="">',
+    dots: true,
+    dotsClass: 'header__slider-dots',
+    //centerMode: true,
+    //focusOnSelect: true
+  });
+
+
+  
+});
